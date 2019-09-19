50ddee043c092c2fa165b8b8f17b918d7b1d9f18
Thomas Jackson <jacksontj.89@gmail.com>
2019-09-19 11:53:29 -0700

    Add test case for add/removes that are close to eachother

--- sync_test.go
+++ sync_test.go
@@ -20,7 +20,7 @@ func equalTargets(a, b []*Target) error {
 	}
 
 	if len(aMap) != len(bMap) {
-		return fmt.Errorf("Mismatch in len")
+		return fmt.Errorf("Mismatch in len a=%d b=%d", len(aMap), len(bMap))
 	}
 
 	for k := range aMap {
@@ -56,6 +56,8 @@ func TestSyncer(t *testing.T) {
 		{IP: "1"},
 		{IP: "2"},
 	}
+	target := []*Target{targets[0]}
+	empty := []*Target{}
 
 	// set targets
 	src.ch <- targets
@@ -69,17 +71,15 @@ func TestSyncer(t *testing.T) {
 
 	time.Sleep(time.Second * 2)
 
-	removed := []*Target{targets[0]}
-	src.ch <- removed
+	src.ch <- target
 	time.Sleep(time.Second * 2)
 
 	// check that they match
 	tgts, _ = dst.GetTargets(nil)
-	if err := equalTargets(removed, tgts); err != nil {
-		t.Fatalf("Mismatch in targets err=%v expected=%+v actual=%+v", err, removed, tgts)
+	if err := equalTargets(target, tgts); err != nil {
+		t.Fatalf("Mismatch in targets err=%v expected=%+v actual=%+v", err, target, tgts)
 	}
 
-	empty := []*Target{}
 	src.ch <- empty
 	time.Sleep(time.Second * 2)
 
@@ -87,5 +87,52 @@ func TestSyncer(t *testing.T) {
 	tgts, _ = dst.GetTargets(nil)
 	if err := equalTargets(empty, tgts); err != nil {
 		t.Fatalf("Mismatch in targets err=%v expected=%+v actual=%+v", err, empty, tgts)
+	}
+}
+
+// TestSyncer_Races is specifically checking for issues where removals and adds are racing with eachother
+func TestSyncer_Races(t *testing.T) {
+	logrus.SetLevel(logrus.DebugLevel)
+	cfg := &SyncConfig{
+		LockOptions: LockOptions{
+			Key: "a",
+			TTL: time.Second,
+		},
+		RemoveDelay: time.Second,
+	}
+
+	src := newmockSource()
+	dst := newmockDestination()
+	syncer := &Syncer{
+		Config: cfg,
+		Locker: &mockLocker{},
+		Src:    src,
+		Dst:    dst,
+	}
+
+	go syncer.Run(context.TODO())
+
+	target := []*Target{{IP: "1"}}
+	empty := []*Target{}
+
+	// Add 1
+	src.ch <- target
+	time.Sleep(time.Second * 2)
+
+	// remove it twice
+	src.ch <- empty
+	time.Sleep(time.Millisecond * 300)
+	src.ch <- empty
+	time.Sleep(time.Millisecond * 300)
+
+	// add 1
+	// sleep 1s
+	src.ch <- target
+	time.Sleep(time.Second * 2)
+
+	// check
+	tgts, _ := dst.GetTargets(nil)
+	if err := equalTargets(target, tgts); err != nil {
+		t.Fatalf("Mismatch in targets err=%v expected=%+v actual=%+v", err, target, tgts)
 	}
 }
